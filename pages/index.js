c131a67bd746b0b06e961adcd61f3e7ed9c65d73
HibaMoideen <hiba.moideen@thefirstgroup.com>
2022-08-24 11:40:34 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ function HomePage() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.cardu}>
             <h2>Paragraph</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed nibh consequat, iaculis nibh ut, pretium tellus. Sed et porta felis. Proin suscipit quam non purus porta, in venenatis est ultricies. Mauris quis massa tincidunt, accumsan ligula vel, imperdiet nunc. Praesent hendrerit ultricies mollis. Fusce faucibus dictum urna, ac viverra sapien. Proin aliquam auctor varius. Aenean volutpat lacinia hendrerit. Vivamus id tristique nunc, ac vulputate enim. In ut lorem nec quam tempus scelerisque a lacinia nunc. Ut vitae libero congue, porta felis ut, cursus ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque sed lectus diam. Ut blandit tristique aliquet. Vivamus dignissim est nec sapien viverra laoreet.</p>
+            <p>Loren ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed nibh consequat, iaculis nibh ut, pretium tellus. Sed et porta felis. Proin suscipit quam non purus porta, in venenatis est ultricies. Mauris quis massa tincidunt, accumsan ligula vel, imperdiet nunc. Praesent hendrerit ultricies mollis. Fusce faucibus dictum urna, ac viverra sapien. Proin aliquam auctor varius. Aenean volutpat lacinia hendrerit. Vivamus id tristique nunc, ac vulputate enim. In ut lorem nec quam tempus scelerisque a lacinia nunc. Ut vitae libero congue, porta felis ut, cursus ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque sed lectus diam. Ut blandit tristique aliquet. Vivamus dignissim est nec sapien viverra laoreet.</p>
           </a></div>
       </main>
       
